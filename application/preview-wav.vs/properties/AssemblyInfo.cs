a92aa23099c94b32124a849d15cb3e5206fbc08b
Viacheslav Lozinskyi <vl.spirit@gmail.com>
2020-12-04 01:41:56 +0200

    - Initial release

---
+++ application/preview-wav.vs/properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle(resource.package.PreviewWAVPackage.CONSTANT.NAME)]
+[assembly: AssemblyDescription(resource.package.PreviewWAVPackage.CONSTANT.DESCRIPTION)]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct(resource.package.PreviewWAVPackage.CONSTANT.NAME)]
+[assembly: AssemblyCopyright(resource.package.PreviewWAVPackage.CONSTANT.COPYRIGHT)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion(resource.package.PreviewWAVPackage.CONSTANT.VERSION)]
+[assembly: AssemblyFileVersion(resource.package.PreviewWAVPackage.CONSTANT.VERSION)]
---
+++ application/preview-wav.vs/resource/package/VSPackage.cs
@@ -0,0 +1,47 @@
+﻿
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace resource.package
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration(CONSTANT.NAME, CONSTANT.DESCRIPTION, CONSTANT.VERSION)]
+    [Guid(CONSTANT.GUID)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.ShellInitialized_string, PackageAutoLoadFlags.BackgroundLoad)]
+    public sealed class PreviewWAVPackage : AsyncPackage
+    {
+        internal static class CONSTANT
+        {
+            public const string COPYRIGHT = "Copyright (c) 2020 by Viacheslav Lozinskyi. All rights reserved.";
+            public const string DESCRIPTION = "Quick preview of WAV files";
+            public const string EXTENSION = ".WAV";
+            public const string GUID = "895957D5-FF20-45A3-B03C-35E6262D2FD6";
+            public const string NAME = "Preview-WAV";
+            public const string VERSION = "1.0.1";
+        }
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            {
+                cartridge.AnyPreview.Connect();
+                cartridge.AnyPreview.Register(cartridge.AnyPreview.MODE.PREVIEW, CONSTANT.EXTENSION, new resource.preview.Taglib());
+            }
+            {
+                await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            }
+        }
+
+        protected override int QueryClose(out bool canClose)
+        {
+            {
+                cartridge.AnyPreview.Disconnect();
+                canClose = true;
+            }
+            return VSConstants.S_OK;
+        }
+    }
+}
